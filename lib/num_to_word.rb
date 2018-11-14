0407a594e92511e510ffa6c22496cb422b684abb
Brooke Perry and Ralph Perdomo <brooke@>
2018-11-14 14:24:25 -0800

    goes up to 100000

--- lib/num_to_word.rb
+++ lib/num_to_word.rb
@@ -5,40 +5,42 @@ def wordifier(num)
   joiner = []
 
 
-  num_words = {"singles" => ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+  num_words = {"ones_teens" => ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
     "tens" => ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
   }
 
-  bank = num.to_s.split("").reverse()
+  reverse_num_array = num.to_s.split("").reverse()
 
-  if (num >= 10000) & (num < 19999)
-  # binding.pry
-    joiner.push(num_words.fetch("singles")[(bank[4]+bank[3]).to_i]+" thousand ")
-    num = (bank[2]+bank[1]+bank[0]+).to_i
-
-    # num = (bank[0]+bank[1]+bank[2]).to_i
+  if (num >= 20000) & (num < 100000)
+    joiner.push(num_words.fetch("tens")[reverse_num_array[4].to_i-2])
+    joiner.push(num_words.fetch("ones_teens")[reverse_num_array[3].to_i] + " thousand ")
+    num = (reverse_num_array[2]+reverse_num_array[1]+reverse_num_array[0]).to_i
   end
 
-
-  if (num >= 1000) & (num < 9999)
-    joiner.push(num_words.fetch("singles")[bank[3].to_i]+" thousand ")
-    num = (bank[0]+bank[1]+bank[2]).to_i
+  if (num >= 10000) & (num < 20000)
+    joiner.push(num_words.fetch("ones_teens")[(reverse_num_array[4]+reverse_num_array[3]).to_i]+" thousand ")
+    num = (reverse_num_array[2]+reverse_num_array[1]+reverse_num_array[0]).to_i
   end
 
+  if (num >= 1000) & (num < 10000)
+    joiner.push(num_words.fetch("ones_teens")[reverse_num_array[3].to_i]+" thousand ")
+    num = (reverse_num_array[2]+reverse_num_array[1]+reverse_num_array[0]).to_i
+  end
 
-  if (num >= 100) & (num < 999)
-    joiner.push(num_words.fetch("singles")[bank[2].to_i]+" hundred ")
-    num = (bank[0]+bank[1]).to_i
+  if (num >= 100) & (num < 1000)
+    joiner.push(num_words.fetch("ones_teens")[reverse_num_array[2].to_i]+" hundred ")
+    num = (reverse_num_array[1]+reverse_num_array[0]).to_i
   end
 
   if (num >= 20) & (num < 100)
-    joiner.push(num_words.fetch("tens")[bank[1].to_i-2])
-    num = bank[0].to_i
+    joiner.push(num_words.fetch("tens")[reverse_num_array[1].to_i-2])
+    joiner.push("-")
+    num = reverse_num_array[0].to_i
   end
 
   if (num<20)
-    joiner.push(num_words.fetch("singles")[num])
-    return joiner.join().sub(/\s$/ , '')
+    joiner.push(num_words.fetch("ones_teens")[num])
+    return joiner.join().sub(/\W*$/ , '')
   else
     return 'too big'
   end
--- spec/num_to_word_spec.rb
+++ spec/num_to_word_spec.rb
@@ -20,17 +20,20 @@ describe('num_to_word') do
   end
 
   it("respond with 'twenty' when user enters '21'") do
-    expect(wordifier(21)).to(eq('twentyone'))
+    expect(wordifier(21)).to(eq('twenty-one'))
   end
 
   it("respond with 'twenty' when user enters '32'") do
-    expect(wordifier(32)).to(eq('thirtytwo'))
+    expect(wordifier(32)).to(eq('thirty-two'))
   end
 
   it("respond with 'twenty' when user enters '99'") do
-    expect(wordifier(99)).to(eq('ninetynine'))
+    expect(wordifier(90)).to(eq('ninety'))
   end
 
+  it("respond with 'twenty' when user enters '99'") do
+    expect(wordifier(99)).to(eq('ninety-nine'))
+  end
 
   it("respond with 'too big' when user enters number greater than '100'") do
     expect(wordifier(100)).to(eq('one hundred'))
@@ -41,35 +44,31 @@ describe('num_to_word') do
   end
 
   it("respond with 'too big' when user enters number greater than '258'") do
-    expect(wordifier(258)).to(eq('two hundred fiftyeight'))
+    expect(wordifier(258)).to(eq('two hundred fifty-eight'))
   end
 
-  it("respond with 'too big' when user enters number greater than '1000'") do
-    expect(wordifier(1000)).to(eq('one thousand'))
+  it("respond with 'too big' when user enters number greater than '990'") do
+    expect(wordifier(990)).to(eq('nine hundred ninety'))
   end
 
-  it("respond with 'too big' when user enters number greater than '2563'") do
-    expect(wordifier(2563)).to(eq('two thousand five hundred sixtythree'))
+  it("respond with 'too big' when user enters number greater than '999'") do
+    expect(wordifier(999)).to(eq('nine hundred ninety-nine'))
   end
 
-  it("respond with 'too big' when user enters number greater than '10000'") do
-    expect(wordifier(10000)).to(eq('ten thousand'))
+  it("respond with 'too big' when user enters number greater than '1999'") do
+    expect(wordifier(1999)).to(eq('one thousand nine hundred ninety-nine'))
   end
 
-  it("respond with 'too big' when user enters number greater than '11894'") do
-    expect(wordifier(11894)).to(eq('eleven thousand eight hundred ninetyfour'))
+  it("respond with 'too big' when user enters number greater than '1999'") do
+    expect(wordifier(9999)).to(eq('nine thousand nine hundred ninety-nine'))
   end
 
-  it("respond with 'too big' when user enters number greater than '19000'") do
-    expect(wordifier(19000)).to(eq('nineteen thousand'))
+  it("respond with 'too big' when user enters number greater than '1999'") do
+    expect(wordifier(20000)).to(eq('twenty thousand'))
   end
 
-  it("respond with 'too big' when user enters number greater than '20000'") do
-    expect(wordifier(19990)).to(eq('two thousand five hundred sixtythree'))
-  end
-
-  it("respond with 'too big' when user enters number greater than '25000'") do
-  expect(wordifier(25000)).to(eq('two thousand five hundred sixtythree'))
+  it("respond with 'too big' when user enters number greater than '1999'") do
+    expect(wordifier(29999)).to(eq('twentynine thousand nine hundred ninety-nine'))
   end
 
 
